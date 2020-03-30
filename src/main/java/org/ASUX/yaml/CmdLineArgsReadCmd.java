a49c03f1271a76acf4bdf07ff1bd18954058d85d
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2020-03-29 23:47:43 -0400

    FEAT: Switched from Apache.commons.cli to ALTLR4-parser successfully

---
+++ src/main/java/org/ASUX/yaml/CmdLineArgsReadCmd.java
@@ -0,0 +1,67 @@
+/*
+ BSD 3-Clause License
+ 
+ Copyright (c) 2019, Udaybhaskar Sarma Seetamraju
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ * Neither the name of the copyright holder nor the names of its
+ contributors may be used to endorse or promote products derived from
+ this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.ASUX.yaml;
+
+/** <p>This class is a typical use of the org.apache.commons.cli package.</p>
+ *  <p>This class has No other function - other than to parse the commandline arguments and handle user's input errors.</p>
+ *  <p>For making it easy to have simple code generate debugging-output, added a toString() method to this class.</p>
+ *  <p>Typical use of this class is: </p>
+ *<pre>
+ public static void main(String[] args) {
+ CmdLineArgsInsertCmd = new CmdLineArgsInsertCmd(args);
+ .. ..
+ *</pre>
+ *
+ *  <p>See full details of how to use this, in {@link org.ASUX.yaml.Cmd} as well as the <a href="https://github.com/org-asux/org.ASUX.cmdline">org.ASUX.cmdline</a> GitHub.com project.</p>
+ * @see org.ASUX.yaml.Cmd
+ */
+public class CmdLineArgsInsertCmd extends CmdLineArgs {
+
+    public static final String CLASSNAME = CmdLineArgsInsertCmd.class.getName();
+
+    public String insertFilePath = null;       // optional argument, but required for 'insert' command
+
+    //=================================================================================
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //=================================================================================
+
+    /** For making it easy to have simple code generate debugging-output, added this toString() method to this class.
+     */
+    @Override
+    public String toString() {
+        return super.toString() +" insertFile="+insertFilePath +" & newContent(not shown).";
+    }
+
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+}
